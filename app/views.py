cdf2a6d34cf7e695385da5001ca383c27c570b26
DimaDK <DimaKrutDK@ya.ru>
2019-01-12 14:39:55 +0300

    feat: initial add order url, view and template

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from app.models import ProductType
+
+
+def add_order_view(request):
+    product_types = ProductType.objects.all()
+    return render(request, 'app/add_order.html', {'types': product_types})
