7e48811ddc94c57c5915f3307a75b1c865cc3dd2
DimaDK <DimaKrutDK@ya.ru>
2019-01-15 14:22:34 +0300

    chore: configure ALLOWED_HOSTS to only localhost, my ip and deployment

--- manager/settings.py
+++ manager/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG')))
 
-ALLOWED_HOSTS = ['127.0.0.1', '*']
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.43.129', 'dimadk.pythonanywhere.com']
 
 # Application definition
 
