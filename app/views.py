010694eb2c9a13c7f1d42d2d85c0149e0b8655f0
DimaDK <DimaKrutDK@ya.ru>
2019-01-15 15:50:25 +0300

    feat: api view for getting product type parameters
    Also, configure PyCharm http client with git ignored env vars

--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 
 from app.models import ProductType
+from app.responses import russian_json_response
 
 
 def index_view(request):
@@ -14,3 +15,40 @@ def index_view(request):
 def add_order_view(request):
     product_types = ProductType.objects.all()
     return render(request, 'app/add_order.html', {'types': product_types})
+
+
+@login_required
+def get_product_type_parameters(request):
+    """
+    returns
+    [
+        {
+            id: number,
+            name: string,
+            options: [
+                {
+                    id: number,
+                    name: string
+                }
+            ]
+        }
+    ]
+    """
+    product_type_id = request.GET['id']
+    product_type = get_object_or_404(ProductType, pk=product_type_id)
+    parameters = product_type.parameters.all()
+    response = []
+    for parameter in parameters:
+        response_parameter = {
+            'id': parameter.pk,
+            'name': parameter.name,
+            'options': []
+        }
+        options = parameter.option_set.all()
+        for option in options:
+            response_parameter['options'].append({
+                'id': option.pk,
+                'name': option.name
+            })
+        response.append(response_parameter)
+    return russian_json_response(response, safe=False)
